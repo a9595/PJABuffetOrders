1886c13e6fbbfc358b5ff6f8a0f193448225ee10
Andrii Kovalchuk <tieorange@gmail.com>
2016-10-31 22:08:58 +0100

    back button

--- app/src/main/java/tieorange/com/pjabuffetorders/activities/ProductActivity.java
+++ app/src/main/java/tieorange/com/pjabuffetorders/activities/ProductActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 import butterknife.BindView;
@@ -38,8 +39,8 @@ public class ProductActivity extends AppCompatActivity {
     setContentView(R.layout.activity_product);
     ButterKnife.bind(this);
     Dart.inject(this);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
+    setSupportActionBar(mToolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     initFAB();
     initViews();
@@ -51,9 +52,20 @@ public class ProductActivity extends AppCompatActivity {
     mTime.setText(String.valueOf(mProduct.cookingTime));
   }
 
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == android.R.id.home) {
+      onBackPressed();
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
   private void initFAB() {
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show());
-    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
   }
 }
