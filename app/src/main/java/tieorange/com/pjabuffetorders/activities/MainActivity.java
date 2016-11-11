ba7fb8332d9ea569ee6fe1d6579502c703de22b8
Andrii Kovalchuk <tieorange@gmail.com>
2016-11-11 22:05:47 +0100

    fixed backStack bug

--- app/src/main/java/tieorange/com/pjabuffetorders/activities/MainActivity.java
+++ app/src/main/java/tieorange/com/pjabuffetorders/activities/MainActivity.java
@@ -21,138 +21,147 @@ import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import tieorange.com.pjabuffetorders.MyApplication;
 import tieorange.com.pjabuffetorders.R;
 import tieorange.com.pjabuffetorders.fragments.OrdersFragment;
 import tieorange.com.pjabuffetorders.fragments.ProductsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-  private static final int ID_PRODUCTS = 1;
-  public static final int ID_ORDERS = 2;
-  @BindView(R.id.fragmentContainer) FrameLayout mFragmentContainer;
-  private Drawer mDrawerBuild;
-  @BindView(R.id.toolbar) public Toolbar mToolbar;
-  private ProductsFragment mProductsFragment;
-  private OrdersFragment mOrdersFragment;
+    private static final int ID_PRODUCTS = 1;
+    public static final int ID_ORDERS = 2;
+    @BindView(R.id.fragmentContainer)
+    FrameLayout mFragmentContainer;
+    private Drawer mDrawerBuild;
+    @BindView(R.id.toolbar)
+    public Toolbar mToolbar;
+    private ProductsFragment mProductsFragment;
+    private OrdersFragment mOrdersFragment;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    ButterKnife.bind(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-    setSupportActionBar(mToolbar);
+        setSupportActionBar(mToolbar);
 
-    initFragments();
-    initDrawer();
-  }
-
-  @Override protected void onResume() {
-    super.onResume();
-    //MyApplication.sFirebaseDatabase.goOnline();
-  }
-
-  @Override protected void onPause() {
-    this.finish();
-    //MyApplication.sFirebaseDatabase.goOffline();
-    super.onPause();
-  }
-
-  private void initFragments() {
-    mProductsFragment = ProductsFragment.newInstance();
-    mOrdersFragment = OrdersFragment.newInstance();
-  }
-
-  private void setFragment(Fragment fragment) {
-    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
-  }
-
-  private void initDrawer() {
-
-    //if you want to update the items at a later time it is recommended to keep it in a variable
-    SecondaryDrawerItem itemProducts =
-        new SecondaryDrawerItem().withIdentifier(ID_PRODUCTS).withName(R.string.products).withIcon(GoogleMaterial.Icon.gmd_local_pizza);
-    SecondaryDrawerItem itemOrders =
-        new SecondaryDrawerItem().withIdentifier(ID_ORDERS).withName(R.string.orders).withIcon(GoogleMaterial.Icon.gmd_shopping_cart);
-    //SecondaryDrawerItem secondaryDrawerItem = new SecondaryDrawerItem().withName(R.string.settings);
-
-    //create the drawer and remember the `Drawer` result object
-    mDrawerBuild = new DrawerBuilder().withActivity(this)
-        .withToolbar(mToolbar)
-        .addDrawerItems(itemProducts, itemOrders)
-        .withOnDrawerItemClickListener(getOnDrawerItemClickListener())
-        .build();
-
-    mDrawerBuild.setSelection(itemOrders);
-  }
-
-  @NonNull private Drawer.OnDrawerItemClickListener getOnDrawerItemClickListener() {
-    return new Drawer.OnDrawerItemClickListener() {
-      @Override public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-        int identifier = (int) drawerItem.getIdentifier();
-        switch (identifier) {
-          case ID_ORDERS:
-            setFragment(mOrdersFragment);
-            setTitle(R.string.orders);
-            break;
-          case ID_PRODUCTS:
-            setFragment(mProductsFragment);
-            setTitle(R.string.products);
-            break;
-
-          default:
-            Toast.makeText(MainActivity.this, "default", Toast.LENGTH_SHORT).show();
-            break;
-        }
-
-        return true;
-      }
-    };
-  }
-
-  @Override public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
+        initFragments();
+        initDrawer();
     }
 
-    return super.onOptionsItemSelected(item);
-  }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MyApplication.sFirebaseDatabase.goOnline();
+    }
 
-  public void writeToDatabase() {
-    // Write a message to the database
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
+    @Override
+    protected void onPause() {
+        MyApplication.sFirebaseDatabase.goOffline();
+        super.onPause();
+    }
 
-    myRef.setValue("Hello, World!");
-  }
+    private void initFragments() {
+        mProductsFragment = ProductsFragment.newInstance();
+        mOrdersFragment = OrdersFragment.newInstance();
+    }
 
-  //public void readFromDatabase(){
-  //  // Read from the database
-  //  myRef.addValueEventListener(new ValueEventListener() {
-  //    @Override
-  //    public void onDataChange(DataSnapshot dataSnapshot) {
-  //      // This method is called once with the initial value and again
-  //      // whenever data at this location is updated.
-  //      String value = dataSnapshot.getValue(String.class);
-  //      Log.d(TAG, "Value is: " + value);
-  //    }
-  //
-  //    @Override
-  //    public void onCancelled(DatabaseError error) {
-  //      // Failed to read value
-  //      Log.w(TAG, "Failed to read value.", error.toException());
-  //    }
-  //  });
-  //}
+    private void setFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
+    }
+
+    private void initDrawer() {
+
+        //if you want to update the items at a later time it is recommended to keep it in a variable
+        SecondaryDrawerItem itemProducts =
+                new SecondaryDrawerItem().withIdentifier(ID_PRODUCTS).withName(R.string.products).withIcon(GoogleMaterial.Icon.gmd_local_pizza);
+        SecondaryDrawerItem itemOrders =
+                new SecondaryDrawerItem().withIdentifier(ID_ORDERS).withName(R.string.orders).withIcon(GoogleMaterial.Icon.gmd_shopping_cart);
+        //SecondaryDrawerItem secondaryDrawerItem = new SecondaryDrawerItem().withName(R.string.settings);
+
+        //create the drawer and remember the `Drawer` result object
+        mDrawerBuild = new DrawerBuilder().withActivity(this)
+                .withToolbar(mToolbar)
+                .addDrawerItems(itemProducts, itemOrders)
+                .withOnDrawerItemClickListener(getOnDrawerItemClickListener())
+                .build();
+
+        mDrawerBuild.setSelection(itemOrders);
+    }
+
+    @NonNull
+    private Drawer.OnDrawerItemClickListener getOnDrawerItemClickListener() {
+        return new Drawer.OnDrawerItemClickListener() {
+            @Override
+            public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                int identifier = (int) drawerItem.getIdentifier();
+                switch (identifier) {
+                    case ID_ORDERS:
+                        setFragment(mOrdersFragment);
+                        setTitle(R.string.orders);
+                        break;
+                    case ID_PRODUCTS:
+                        setFragment(mProductsFragment);
+                        setTitle(R.string.products);
+                        break;
+
+                    default:
+                        Toast.makeText(MainActivity.this, "default", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        };
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void writeToDatabase() {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+
+    //public void readFromDatabase(){
+    //  // Read from the database
+    //  myRef.addValueEventListener(new ValueEventListener() {
+    //    @Override
+    //    public void onDataChange(DataSnapshot dataSnapshot) {
+    //      // This method is called once with the initial value and again
+    //      // whenever data at this location is updated.
+    //      String value = dataSnapshot.getValue(String.class);
+    //      Log.d(TAG, "Value is: " + value);
+    //    }
+    //
+    //    @Override
+    //    public void onCancelled(DatabaseError error) {
+    //      // Failed to read value
+    //      Log.w(TAG, "Failed to read value.", error.toException());
+    //    }
+    //  });
+    //}
 }
